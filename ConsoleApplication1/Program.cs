d5c4c23de52b8d1d4f18a9a0e1d101fe4f862e73
lesterxu <lester88a@gmail.com>
2016-04-26 17:41:54 -0400

    add ip changes detect

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Net;
+using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,20 +17,32 @@ namespace ConsoleApplication1
             //display the IP
             Console.WriteLine(GetLocalIPAddress());
 
-
+            Console.WriteLine("Starting kill the application...");
             //kill the process
             try
             {
                 foreach (Process proc in Process.GetProcessesByName("notepad"))
                 {
-                    proc.Kill();
+                    if (proc.ProcessName=="notepad")
+                    {
+                        proc.Kill();
+                        Console.WriteLine("The application terminate successful!");
+                    }
                 }
             }
             catch (Exception ex)
             {
+                Console.WriteLine("Sorry, see this following errors:");
                 Console.WriteLine(ex); ;
             }
+
+            //detect IP changes
+            NetworkChange.NetworkAddressChanged += new
+            NetworkAddressChangedEventHandler(AddressChangedCallback);
+            Console.WriteLine("Listening for address changes. Press any key to exit.");
+            Console.ReadLine();
         }
+        //get local IP
         public static string GetLocalIPAddress()
         {
             var host = Dns.GetHostEntry(Dns.GetHostName());
@@ -41,6 +54,23 @@ namespace ConsoleApplication1
                 }
             }
             throw new Exception("Local IP Address Not Found!");
+        }
+
+        //listening IP changes
+        static void AddressChangedCallback(object sender, EventArgs e)
+        {
+
+            NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (NetworkInterface n in adapters)
+            {
+                string currentIP = GetLocalIPAddress();
+                Console.WriteLine(currentIP);
+
+                if (currentIP.Contains("10.12.0"))
+                {
+                    Console.WriteLine("KILLLLLL");
+                }
+            }
         }
     }
 }
